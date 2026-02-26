a1320ed8188877f88e5e83c5150825b95c7daa5a
Rohit Kumar <rk4840369@gmail.com>
2026-02-26 14:33:23 +0530

    Starting of react

---
+++ ReactJs/01-folder/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+        <h2>Hello</h2>
+        <h3>Hello</h3>
+        <h4>Hello</h4>
+        <h5>Hello</h5>
+      </div>
+      <div id="Chacha">
+
+      </div>
+    </>
+  );
+};
+
+export default App;
